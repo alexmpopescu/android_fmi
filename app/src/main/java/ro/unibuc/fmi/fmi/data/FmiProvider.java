06edebe208159bc30804169e86031f8a61951299
Alexandru Popescu <alexpop04@gmail.com>
2016-04-09 10:16:12 +0300

    Finished FmiProvider

--- app/src/main/java/ro/unibuc/fmi/fmi/data/FmiProvider.java
+++ app/src/main/java/ro/unibuc/fmi/fmi/data/FmiProvider.java
@@ -1,5 +1,6 @@
 package ro.unibuc.fmi.fmi.data;
 
+import android.annotation.TargetApi;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
@@ -19,8 +20,10 @@ public class FmiProvider extends ContentProvider {
     private FmiDbHelper fmiDbHelper;
 
     static final int CATEGORY = 100;
-    static final int CATEGORY_WITH_POSTS = 101;
+    static final int CATEGORY_WITH_TRANSLATION = 101;
+    static final int POSTS_WITH_TRANSLATION_BY_CATEGORY = 202;
     static final int POST = 200;
+    static final int POST_WITH_TRANSLATION = 201;
     static final int TRANSLATION = 300;
     static final int STRING = 400;
 
@@ -29,8 +32,13 @@ public class FmiProvider extends ContentProvider {
         final String authority = FmiContract.CONTENT_AUTHORITY;
 
         matcher.addURI(authority, FmiContract.PATH_CATEGORY, CATEGORY);
-        matcher.addURI(authority, FmiContract.PATH_CATEGORY + "/*", CATEGORY_WITH_POSTS);
-        matcher.addURI(authority, FmiContract.PATH_POST + "/*", POST);
+        matcher.addURI(authority, FmiContract.PATH_POST, POST);
+        matcher.addURI(authority, FmiContract.PATH_STRING, STRING);
+        matcher.addURI(authority, FmiContract.PATH_TRANSLATION, TRANSLATION);
+
+        matcher.addURI(authority, FmiContract.PATH_CATEGORY + "/*", CATEGORY_WITH_TRANSLATION);
+        matcher.addURI(authority, FmiContract.PATH_CATEGORY + "/*/*", POSTS_WITH_TRANSLATION_BY_CATEGORY);
+        matcher.addURI(authority, FmiContract.PATH_POST + "/*/*", POST_WITH_TRANSLATION);
 
         return matcher;
     }
@@ -45,26 +53,72 @@ public class FmiProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
-        Cursor cursor;
+        Cursor retCursor;
+        String locale;
+        String _id;
         switch (uriMatcher.match(uri)) {
             case CATEGORY:
-                cursor = getCategories(selectionArgs);
-                break;
-            case CATEGORY_WITH_POSTS:
-                cursor = getCategoryWithPosts(uri, selectionArgs);
+                retCursor = fmiDbHelper.getReadableDatabase().query(
+                        CategoryEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
                 break;
             case POST:
-                cursor = getPost(uri, selectionArgs);
+                retCursor = fmiDbHelper.getReadableDatabase().query(
+                        PostEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case TRANSLATION:
+                retCursor = fmiDbHelper.getReadableDatabase().query(
+                        TranslationEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case STRING:
+                retCursor = fmiDbHelper.getReadableDatabase().query(
+                        StringEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case CATEGORY_WITH_TRANSLATION:
+                locale = uri.getPathSegments().get(1);
+                retCursor = getCategories(locale);
+                break;
+            case POSTS_WITH_TRANSLATION_BY_CATEGORY:
+                locale = uri.getPathSegments().get(1);
+                _id = uri.getPathSegments().get(2);
+                retCursor = getCategoryWithPosts(_id, locale);
+                break;
+            case POST_WITH_TRANSLATION:
+                locale = uri.getPathSegments().get(1);
+                _id = uri.getPathSegments().get(2);
+                retCursor = getPost(_id, locale);
                 break;
             default:
                 throw new UnsupportedOperationException("Unknown uri " + uri);
         }
-        cursor.setNotificationUri(getContext().getContentResolver(), uri);
-        return cursor;
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 
-    private Cursor getPost(Uri uri, String[] selectionArgs) {
-        String postId = uri.getPathSegments().get(1);
+    private Cursor getPost(String _id, String locale) {
         String sql = "SELECT p1." + PostEntry._ID + ", t1." + TranslationEntry.COLUMN_VALUE +
                 " AS " + PostEntry.COLUMN_TITLE_STRING_KEY + ", (SELECT t2." +
                 TranslationEntry.COLUMN_VALUE + " FROM " + PostEntry.TABLE_NAME + " p2 JOIN " +
@@ -78,11 +132,10 @@ public class FmiProvider extends ContentProvider {
                 TranslationEntry.COLUMN_LOCALE + " = ?";
 
         return fmiDbHelper.getReadableDatabase().rawQuery(sql,
-                new String[] {postId, selectionArgs[0]});
+                new String[] {_id, locale});
     }
 
-    private Cursor getCategoryWithPosts(Uri uri, String[] selectionArgs) {
-        String categoryId = uri.getPathSegments().get(1);
+    private Cursor getCategoryWithPosts(String _id, String locale) {
         String sql = "SELECT p1." + PostEntry._ID + ", t1." + TranslationEntry.COLUMN_VALUE +
                 " AS " + PostEntry.COLUMN_TITLE_STRING_KEY + " FROM " + PostEntry.TABLE_NAME +
                 " p1 JOIN " + CategoryEntry.TABLE_NAME + " c1 ON p1." + PostEntry.COLUMN_CATEGORY_KEY +
@@ -91,10 +144,10 @@ public class FmiProvider extends ContentProvider {
                 " WHERE c1." + CategoryEntry._ID + " = ? AND t1." + TranslationEntry.COLUMN_LOCALE + " = ?";
 
         return fmiDbHelper.getReadableDatabase().rawQuery(sql,
-                new String[] {categoryId, selectionArgs[0]});
+                new String[] {_id, locale});
     }
 
-    private Cursor getCategories(String[] selectionArgs) {
+    private Cursor getCategories(String locale) {
 
         String sql = "SELECT c1." + CategoryEntry._ID + ", t1." + TranslationEntry.COLUMN_VALUE +
                 " AS " + CategoryEntry.COLUMN_NAME_STRING_KEY + " FROM " + CategoryEntry.TABLE_NAME +
@@ -102,7 +155,7 @@ public class FmiProvider extends ContentProvider {
                 CategoryEntry.COLUMN_NAME_STRING_KEY + " = t1." + TranslationEntry.COLUMN_STRING_KEY +
                 " WHERE t1." + TranslationEntry.COLUMN_LOCALE + " = ?" ;
 
-        return fmiDbHelper.getReadableDatabase().rawQuery(sql, selectionArgs);
+        return fmiDbHelper.getReadableDatabase().rawQuery(sql, new String[]{locale});
     }
 
     @Nullable
@@ -113,10 +166,17 @@ public class FmiProvider extends ContentProvider {
 
         switch (match) {
             case CATEGORY:
-            case CATEGORY_WITH_POSTS:
+            case CATEGORY_WITH_TRANSLATION:
                 return  CategoryEntry.CONTENT_TYPE;
             case POST:
+            case POSTS_WITH_TRANSLATION_BY_CATEGORY:
+                return PostEntry.CONTENT_TYPE;
+            case POST_WITH_TRANSLATION:
                 return PostEntry.CONTENT_ITEM_TYPE;
+            case STRING:
+                return StringEntry.CONTENT_TYPE;
+            case TRANSLATION:
+                return TranslationEntry.CONTENT_TYPE;
             default:
                 throw new UnsupportedOperationException("Unknown uri " + uri);
         }
@@ -162,17 +222,72 @@ public class FmiProvider extends ContentProvider {
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
+
         getContext().getContentResolver().notifyChange(uri, null);
         return returnUri;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        final SQLiteDatabase db = fmiDbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        int deletedRows;
+
+        if (null == selection) selection = "1";
+        switch (match) {
+            case STRING:
+                deletedRows = db.delete(StringEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case TRANSLATION:
+                deletedRows = db.delete(TranslationEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case CATEGORY:
+                deletedRows = db.delete(CategoryEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case POST:
+                deletedRows = db.delete(PostEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (deletedRows > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+        return deletedRows;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        final SQLiteDatabase db = fmiDbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        int updatedRows;
+
+        switch (match) {
+            case STRING:
+                updatedRows = db.update(StringEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case TRANSLATION:
+                updatedRows = db.update(TranslationEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case CATEGORY:
+                updatedRows = db.update(CategoryEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case POST:
+                updatedRows = db.update(PostEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (updatedRows > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+        return updatedRows;
+    }
+
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        fmiDbHelper.close();
+        super.shutdown();
     }
 }
